9d13bb4e0ad9cc57143c41771873827dd85444af
xdanx3 <107212732+xdanx3@users.noreply.github.com>
2025-06-01 21:31:34 +0000

    added stir option

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
+import argparse
+
 def stir_the_void():
-    print("The void stares back...")
+    print("The void stares back... But nothing happens")
 
 if __name__ == "__main__":
-    stir_the_void
+    parser = 
+argparse.ArgumentParser(description="VoidSpoon CLI")
+    parser.add_argument("--stir:,
+action="store true", help="Stir the void")
+    args = parser.parse_args()
+
+    if args.stir:
+        stir_the_void
+    else:
+        print("The void awaits...")
