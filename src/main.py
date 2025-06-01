b7927525df1ba2e9c7894fcbd8a298b72d7f93b4
xdanx3 <107212732+xdanx3@users.noreply.github.com>
2025-06-01 21:58:57 +0000

    README edit

--- src/main.py
+++ src/main.py
@@ -6,12 +6,9 @@ def stir_the_void():
     print("The void stares back... But nothing happens")
 
 if __name__ == "__main__":
-    parser = 
-argparse.ArgumentParser(description="VoidSpoon CLI")
-    parser.add_argument("--stir:,
-action="store true", help="Stir the void")
+    parser = argparse.ArgumentParser(description="VoidSpoon CLI")
+    parser.add_argument("--stir", action="store true", help="Stir the void")
     args = parser.parse_args()
-
     if args.stir:
         stir_the_void
     else:
